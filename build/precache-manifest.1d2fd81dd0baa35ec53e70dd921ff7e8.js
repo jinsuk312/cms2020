03883f6178c2733d3f41813848d94436f2141094
Justin Jinsuk Kim <jinsukdev@gmail.com>
2020-07-22 16:01:25 -0500

    prototype 2

---
+++ build/precache-manifest.1d2fd81dd0baa35ec53e70dd921ff7e8.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486b7cf825cec406626750498a000244",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88129a00a39fa9d15620",
+    "url": "/static/css/2.624f0c3e.chunk.css"
+  },
+  {
+    "revision": "66202e01d48d70838d3d",
+    "url": "/static/css/main.4fe3cf2f.chunk.css"
+  },
+  {
+    "revision": "88129a00a39fa9d15620",
+    "url": "/static/js/2.092f0f3e.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/static/js/2.092f0f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66202e01d48d70838d3d",
+    "url": "/static/js/main.511ca769.chunk.js"
+  },
+  {
+    "revision": "2b8f580612459d80dc88",
+    "url": "/static/js/runtime-main.f65105ae.js"
+  },
+  {
+    "revision": "45262beaa91f936e55f213d0ee39dd59",
+    "url": "/static/media/THEANSWER.45262bea.mp3"
+  },
+  {
+    "revision": "b756f718ef731cb2903ab2707ab1ca24",
+    "url": "/static/media/bg7.b756f718.png"
+  },
+  {
+    "revision": "12b498c8a9e155afd871225aa6105287",
+    "url": "/static/media/brow-gallery1.12b498c8.jpeg"
+  },
+  {
+    "revision": "9dd4cc67ff2519fa98e52e58fa10847d",
+    "url": "/static/media/brow-gallery2.9dd4cc67.jpeg"
+  },
+  {
+    "revision": "551d2889cba04a6bb9e63ac93ff8e051",
+    "url": "/static/media/brow1.551d2889.jpg"
+  },
+  {
+    "revision": "f1fe5d7ec83a8903f9a7666bb8638152",
+    "url": "/static/media/brows-gallery5.f1fe5d7e.jpeg"
+  },
+  {
+    "revision": "e775670fbad7e907cdc5b82703d32106",
+    "url": "/static/media/brows-gallery6.e775670f.jpeg"
+  },
+  {
+    "revision": "eab76c6701f13c95a51b73dbd85f1024",
+    "url": "/static/media/brows-gallery7.eab76c67.jpeg"
+  },
+  {
+    "revision": "8f92f35dfe4582b2e960c96b41e2f586",
+    "url": "/static/media/brows-gallery8.8f92f35d.jpeg"
+  },
+  {
+    "revision": "63d9d50651e9c3c5c90644f02b32bb1f",
+    "url": "/static/media/brows-service.63d9d506.jpg"
+  },
+  {
+    "revision": "52bd2380cf107da78ded62646bf09c96",
+    "url": "/static/media/cards.52bd2380.svg"
+  },
+  {
+    "revision": "b236ac0c99e4f522eeb392d007e772e3",
+    "url": "/static/media/carecredit-logo.b236ac0c.svg"
+  },
+  {
+    "revision": "8c4f3e25afb7f0e5f02e66618519eda5",
+    "url": "/static/media/content-bg.8c4f3e25.jpg"
+  },
+  {
+    "revision": "71c3580bec64f85b6d80688d40a76592",
+    "url": "/static/media/final-eyeliner1.71c3580b.jpeg"
+  },
+  {
+    "revision": "ab6d658f846e37992edcd778e67fc3f7",
+    "url": "/static/media/final-lash1.ab6d658f.jpeg"
+  },
+  {
+    "revision": "95b149b517269cab38fc1f464327ae53",
+    "url": "/static/media/final-lips2.95b149b5.jpeg"
+  },
+  {
+    "revision": "d6cbc5075747a8042054fb691422453e",
+    "url": "/static/media/final-micro2.d6cbc507.jpeg"
+  },
+  {
+    "revision": "21f3b47075f40283a6649495fc35b596",
+    "url": "/static/media/final-perm1.21f3b470.jpeg"
+  },
+  {
+    "revision": "e6b9ad3681598e16dc0459e9a1647b78",
+    "url": "/static/media/gallery-extra.e6b9ad36.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "7e8a3f7acd0a6565c4b72f2d7532327e",
+    "url": "/static/media/ig.7e8a3f7a.png"
+  },
+  {
+    "revision": "b1b6bd33720d43b2f3bf95006ed68892",
+    "url": "/static/media/lash-ext-service.b1b6bd33.jpg"
+  },
+  {
+    "revision": "f16684a741bd0a1f6dbf543d0121bab0",
+    "url": "/static/media/lashext.f16684a7.jpeg"
+  },
+  {
+    "revision": "a05866b20c0c456e0f9349d817ff3518",
+    "url": "/static/media/lashext2.a05866b2.jpg"
+  },
+  {
+    "revision": "25e1e0b33bec786c80cfc2d886d7eab4",
+    "url": "/static/media/lashlift.25e1e0b3.jpg"
+  },
+  {
+    "revision": "bf1be9d35481e87bd9ed5d7556699eeb",
+    "url": "/static/media/lashperm3.bf1be9d3.jpg"
+  },
+  {
+    "revision": "bf7a00dd73c951067d4bc6d608d180b2",
+    "url": "/static/media/mens2.bf7a00dd.jpeg"
+  },
+  {
+    "revision": "bac93d21135e64cf6e0bd725c8858867",
+    "url": "/static/media/nlt.bac93d21.jpeg"
+  },
+  {
+    "revision": "7e62893b615ddf7b8520eb0634edc9d0",
+    "url": "/static/media/ombre.7e62893b.jpeg"
+  },
+  {
+    "revision": "367fe1a474f7b2e11cb2cb1741d0d4ef",
+    "url": "/static/media/perm-service.367fe1a4.jpg"
+  },
+  {
+    "revision": "c941f7365a32e62d4aab2941a46cb0d2",
+    "url": "/static/media/perm2.c941f736.jpg"
+  },
+  {
+    "revision": "36b5f0d7e8bc36331773e85fd8ce99c9",
+    "url": "/static/media/perm3.36b5f0d7.jpeg"
+  },
+  {
+    "revision": "279bcd487a57f37c64937aabb7e3774f",
+    "url": "/static/media/phibrows.279bcd48.jpeg"
+  },
+  {
+    "revision": "b97fe467c6abe90aba4a108a20f12025",
+    "url": "/static/media/phibrows2.b97fe467.jpeg"
+  },
+  {
+    "revision": "b45fff330c92a18c81709237baf8f5aa",
+    "url": "/static/media/phibrows3.b45fff33.jpeg"
+  },
+  {
+    "revision": "e7bdd3b0a2f283ef7afcb7d1be73aae5",
+    "url": "/static/media/phicontour.e7bdd3b0.jpeg"
+  },
+  {
+    "revision": "1c34f003af775458813910faaf1e0599",
+    "url": "/static/media/phicontour2.1c34f003.jpeg"
+  },
+  {
+    "revision": "eb0a9c4c742f43b19fa74104820dd4dc",
+    "url": "/static/media/powbrows.eb0a9c4c.jpeg"
+  },
+  {
+    "revision": "f82c543f02150689ac6df66153325a8e",
+    "url": "/static/media/selfie.f82c543f.jpg"
+  },
+  {
+    "revision": "3199284f2484e78dc831d4b4d3d00164",
+    "url": "/static/media/tattoo1.3199284f.jpg"
+  },
+  {
+    "revision": "0d2513f10450b55e05b6a7c066d73ca4",
+    "url": "/static/media/tattoo2.0d2513f1.jpg"
+  },
+  {
+    "revision": "75c31d0a0221442150a50050e20a0634",
+    "url": "/static/media/transparent.75c31d0a.png"
+  },
+  {
+    "revision": "9ea57640dddc27ac8e1925e9f9ae27a7",
+    "url": "/static/media/yelp.9ea57640.png"
+  }
+]);
